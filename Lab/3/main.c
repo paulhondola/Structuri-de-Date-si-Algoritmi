f679e9c339ec985b2ccc1901a1dfda086a7131fe
Paul Hondola <paulhondola@gmail.com>
2024-10-18 19:18:18 +0300

    updated lab 3

--- Lab/3/main.c
+++ Lab/3/main.c
@@ -1,26 +1,20 @@
-#include  "timer.h"
+#include "timer.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void functie_test()
-{
-int i,j,k; 
-for(i=1;i<=200000;i++) 
-	for(j=1;j<=20000;j++) 
-		if(i%2)k=i; 
+void functie_test() {
+  int i, j, k;
+  for (i = 1; i <= 200000; i++)
+    for (j = 1; j <= 20000; j++)
+      if (i % 2)
+        k = i;
 }
 
-
-
-
-
-
-void main(void)
-{ float timp;
-generate_a();
-starton(); 
-functie_test();
-timp=startoff(); 
-printf("%f",timp);
-} 
+int main(void) {
+  float timp;
+  starton();
+  functie_test();
+  timp = startoff();
+  printf("%f", timp);
+  return 0;
+}
--- Lab/3/timer.c
+++ Lab/3/timer.c
@@ -1,10 +1,6 @@
 /* fisierul timer.c */
-#include <time.h>
 #include "timer.h"
-clock_t aux; 
-void starton(void)
-{ aux=clock(); 
-} 
-float startoff(void)
-{ return(clock()-(float)aux)/CLK_TCK;
-} 
+#include <time.h>
+clock_t aux;
+void starton(void) { aux = clock(); }
+float startoff(void) { return (clock() - (float)aux) / CLOCKS_PER_SEC; }
